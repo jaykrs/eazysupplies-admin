05c3297f8706a4ca3f8cc84c54e69d288ed02ff2
Jayant Kumar <jaykrs@gmail.com>
2025-09-06 19:45:39 +0530

    logo fix

--- src/components/commonComponent/logoWrapper/Logo.js
+++ src/components/commonComponent/logoWrapper/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
   const { state, settingObj } = useContext(SettingContext);
   return (
     <Link href="/dashboard">
-      {state?.setLightLogo?.original_url ? <Image className="for-white" src={`${state?.setLightLogo?.original_url || null}`} alt="Light Logo" width={1300} height={500} priority /> : <h2 className="text-white">{settingObj?.general?.site_name || 'Logo Here'}</h2>}
+      {state?.setLightLogo?.original_url ? <Image className="for-white" src={`${state?.setLightLogo?.original_url || "/assets/images/logo-white.png"}`} alt="Light Logo" width={1300} height={500} priority /> : <h2 className="text-white">{settingObj?.general?.site_name || 'Logo Here'}</h2>}
     </Link>
   );
 };
