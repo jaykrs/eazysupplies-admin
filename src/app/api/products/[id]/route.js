89bc70eddf0e64da512b261b1162a03e3508b440
Jayant Kumar <jaykrs@gmail.com>
2025-09-19 20:24:48 +0530

    product detail api fix

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -5,8 +5,9 @@ const prisma = new PrismaClient();
 export async function GET(request, { params }) {
 try {
     const id = (await params).id;
+    console.log(id);
 const product = await prisma.product.findUnique({ where: { id: Number(id) },
-include: { category: true, brand: true, tags: { include: { tag: true } } },
+include: { category: true, brand: true },
 });
 return NextResponse.json(product);
 } catch (Error) {
